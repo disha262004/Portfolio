06f733c22a4d9d02c5cddcd53f7585b44cfa0db6
Disha Rathore <disharathore126@gmail.com>
2025-02-23 00:51:44 +0530

    Education section

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -7,15 +7,15 @@ function Education({
     percentage
     }) {
     return (  
-            <div className="row my-4 p-3" style={{backgroundColor:"#e5e7eb", borderRadius:"10px"}}>
-                <div className="col-1">
-                    <img src={imageUrl} alt="logo" />
+            <div className="row my-4 py-3" style={{backgroundColor:"#e5e7eb", borderRadius:"10px"}}>
+                <div className="col-1 my-1 p-3">
+                    <img src={imageUrl} alt="logo" style={{borderRadius:"100%", width:"100%"}}/>
                 </div>
                 <div className="col-11">
-                    <h4 className="fw-bold">{name}</h4>
+                    <h4 className="fw-bold" style={{color:"#73C7C7"}}>{name}</h4>
                     <h5>{course}</h5>
-                    <p className="text-muted">{period}</p>
-                    <p className="fw-bold text-muted">{percentage}</p>
+                    <p className="text-muted">{period}<br />
+                    {percentage}</p>
                 </div>   
             </div>
     );
--- src/components/Education/Educations.js
+++ src/components/Education/Educations.js
@@ -6,14 +6,14 @@ function Educations() {
         <div className="container mt-5 py-5">
         <h2 className="fw-bold text-center my-5">Education</h2>
             <Education 
-                // imageUrl=""
+                imageUrl="media\images\ju.png"
                 name="Jagannath University, Jaipur" 
                 course="Bachelor of Technology in Computer Science" 
                 period="Expected Graduation: July 2026"
                 percentage="CGPA: 8.0/10"
             />
             <Education 
-                // imageUrl=""
+                imageUrl="media\images\kv.jpeg"
                 name="Kendriya Vidyalaya No.1 Neemuch (M.P)" 
                 course="Se" 
                 period="Expected Graduation: July 2026"
