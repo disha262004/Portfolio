4ec8811f5f36f0e804655c6e0e02d5d72c985d52
Disha Rathore <disharathore126@gmail.com>
2025-02-24 02:22:36 +0530

    Education section

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,24 +1,23 @@
-import React from 'react'
-function Education({
-    imageUrl,
-    name , 
-    course , 
-    period , 
-    percentage
-    }) {
-    return (  
-            <div className="row my-4 py-3" style={{backgroundColor:"#e5e7eb", borderRadius:"10px"}}>
-                <div className="col-1 my-1 p-3">
-                    <img src={imageUrl} alt="logo" style={{borderRadius:"100%", width:"100%"}}/>
-                </div>
-                <div className="col-11">
-                    <h4 className="fw-bold" style={{color:"#73C7C7"}}>{name}</h4>
-                    <h5>{course}</h5>
-                    <p className="text-muted">{period}<br />
-                    {percentage}</p>
-                </div>   
-            </div>
-    );
+import React from "react";
+import './Education.css';
+
+function Education({ imageUrl, name, course, period, percentage }) {
+  return (
+    <div className="education-container row my-4 py-3">
+      <div className="col-1 my-1 p-3">
+        <img src={imageUrl} alt="logo" className="education-logo" />
+      </div>
+      <div className="col-11">
+        <h4 className="education-name fw-bold">{name}</h4>
+        <h5>{course}</h5>
+        <p className="text-muted">
+          {period}
+          <br />
+          {percentage}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Education;
--- src/components/Education/Educations.js
+++ src/components/Education/Educations.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import Education from './Education'
+import React from 'react';
+import Education from './Education';
 
 function Educations() {
-    return ( 
-        <div className="container mt-5 py-5">
-        <h2 className="fw-bold text-center my-5">Education</h2>
-            <Education 
-                imageUrl="media\images\ju.png"
-                name="Jagannath University, Jaipur" 
-                course="Bachelor of Technology in Computer Science" 
-                period="Expected Graduation: July 2026"
-                percentage="CGPA: 8.0/10"
-            />
-            <Education 
-                imageUrl="media\images\kv.jpeg"
-                name="Kendriya Vidyalaya No.1 Neemuch (M.P)" 
-                course="Se" 
-                period="Expected Graduation: July 2026"
-                percentage="Percentage: 83.4%"
-            />
-        </div>
-    
-     );
+  return (
+    <div className="container mt-5 py-5" id="education">
+      <h2 className="fw-bold text-center my-5">Education</h2>
+      <Education 
+        imageUrl="/media/images/ju.png"
+        name="Jagannath University, Jaipur" 
+        course="Bachelor of Technology in Computer Science" 
+        period="Expected Graduation: July 2026"
+        percentage="CGPA: 8.0/10"
+      />
+      <Education 
+        imageUrl="/media/images/kv.jpeg"
+        name="Kendriya Vidyalaya No.1 Neemuch (M.P)" 
+        course="Senior Secondary Education (Class 12)" 
+        period="Graduated: 2022"
+        percentage="Percentage: 83.4%"
+      />
+    </div>
+  );
 }
 
 export default Educations;
