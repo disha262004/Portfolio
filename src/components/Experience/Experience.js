f11cd219a5fd03f425cf361299232422c9f37265
Disha Rathore <disharathore126@gmail.com>
2025-02-24 02:26:11 +0530

    Experience Section

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,33 @@
-import React from 'react'
-function Experince() {
-    return ( 
-        <h1>Experince</h1>
-     );
+import React from "react";
+function Experience({ imageUrl, name, position, period, percentage }) {
+  return (
+    <div
+      className="container row m-3 p-3"
+      style={{ backgroundColor: "#e5e7eb", borderRadius: "10px" }}
+    >
+      <div className="col-1 my-1 p-3">
+        <img
+          src={imageUrl}
+          alt="logo"
+          style={{ borderRadius: "100%", width: "100%" }}
+        />
+      </div>
+      <div className="col-11">
+        <h4
+          className="fw-bold"
+          style={{
+            background: "linear-gradient(90deg, #34d399, #3b82f6)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          {name}
+        </h4>
+        <h5>{position}</h5>
+        <p className="text-muted">{period}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Experince;
+export default Experience;
