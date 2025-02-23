04f1305c44c1dd99629d6f6d9e8b9a14260b934a
Disha Rathore <disharathore126@gmail.com>
2025-02-24 02:24:49 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,59 +3,54 @@ function Navbar() {
   return (
     <>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container mt-3">
+        <div class="container mt-2">
           <a class="navbar-brand fs-3" href="#">
             <b>Disha Rathore</b>
           </a>
-          <div class="" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-5 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link">
+          <div class="space" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-5 mb-lg-0" >
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#hero">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#about">
                   About Me
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" >
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#education">
                   Education
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#skills">
                   Skills
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#experience">
                   Experience
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#projects">
                   Projects
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#certification">
                   Certifications
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  Achievements
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="#contact">
                   Contact
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link"><i class="fa-solid fa-moon"></i></a>
+              <li class="nav-item mx-4">
+                <a class="nav-link"><i class="fa-5 fa-solid fa-moon" style={{width:"100%"}}></i></a>
               </li>
             </ul>
           </div>
